6f3a5f288e1996e005b95e6aa69aadbe90e470ac
738NGX <jny738ngx@gmail.com>
2025-08-11 10:16:50 +0800

    Feat rect transition (#16)
    * support rect
    
    * fix scale-rotate
    
    * bump version

--- src/plugins/elements/RectRendererPlugin.js
+++ src/plugins/elements/RectRendererPlugin.js
@@ -1,5 +1,6 @@
 import { Container, Graphics } from "pixi.js";
 import { TransitionEvent, BaseRendererPlugin } from "../../types";
+import { from, mergeMap, Observable } from "rxjs";
 
 /**
  * @typedef {import('../../types').ContainerElement} ContainerElement
@@ -32,93 +33,111 @@ export class RectRendererPlugin {
    * @param {RectContainerElement} options.element
    * @param {BaseTransition[]} [options.transitions=[]]
    * @param {Function} options.getTransitionByType
-   * @returns {Promise<undefined>}
+   * @returns {Observable<any>}
    */
-  add = async (app, options) => {
-    const {
-      parent,
-      element,
-      transitions = [],
-      getTransitionByType,
-      eventHandler,
-    } = options;
+  add = (app, options) => {
+    return new Observable((observer) => {
+      const {
+        parent,
+        element,
+        transitions = [],
+        getTransitionByType,
+        eventHandler,
+      } = options;
 
-    const graphics = new Graphics();
-    graphics.label = element.id;
+      const graphics = new Graphics();
+      graphics.label = element.id;
 
-    if (element.x !== undefined) {
-      graphics.x = element.x;
-    }
-    if (element.y !== undefined) {
-      graphics.y = element.y;
-    }
-    if (element.alpha !== undefined) {
-      graphics.alpha = element.alpha;
-    }
-    if (element.scaleX !== undefined) {
-      graphics.scale.x = element.scaleX;
-    }
-    if (element.scaleY !== undefined) {
-      graphics.scale.y = element.scaleY;
-    }
-    if (element.rotation !== undefined) {
-      graphics.rotation = (element.rotation * Math.PI) / 180;
-    }
+      if (element.x !== undefined) {
+        graphics.x = element.x;
+      }
+      if (element.y !== undefined) {
+        graphics.y = element.y;
+      }
+      if (element.alpha !== undefined) {
+        graphics.alpha = element.alpha;
+      }
+      if (element.scaleX !== undefined) {
+        graphics.scale.x = element.scaleX;
+      }
+      if (element.scaleY !== undefined) {
+        graphics.scale.y = element.scaleY;
+      }
+      if (element.rotation !== undefined) {
+        graphics.rotation = (element.rotation * Math.PI) / 180;
+      }
 
-    const width = element.width;
-    const height = element.height;
-    graphics.rect(0, 0, width, height);
-    graphics.fill(element.fill);
+      const width = element.width;
+      const height = element.height;
+      graphics.rect(0, 0, width, height);
+      graphics.fill(element.fill);
 
-    if (element.anchorX !== undefined) {
-      graphics.pivot.x = width * element.anchorX;
-    }
-    if (element.anchorY !== undefined) {
-      graphics.pivot.y = height * element.anchorY;
-    }
+      if (element.anchorX !== undefined) {
+        graphics.pivot.x = width * element.anchorX;
+      }
+      if (element.anchorY !== undefined) {
+        graphics.pivot.y = height * element.anchorY;
+      }
 
-    if (
-      (element.clickEventName || element.rightClickEventName) &&
-      eventHandler
-    ) {
-      graphics.eventMode = "static";
-      graphics.on("pointerup", (e) => {
-        if (e.button === 0) {
-          eventHandler(element.clickEventName);
-        } else if (e.button === 2) {
-          eventHandler(element.rightClickEventName);
-        }
-      });
-    }
-
-    if (element.wheelEventName && eventHandler) {
-      graphics.eventMode = "static";
-      graphics.on("wheel", (e) => {
-        eventHandler(element.wheelEventName, {
-          deltaY: e.deltaY,
-        });
-      });
-    }
-
-    const transitionPromises = [];
-
-    for (const transition of transitions) {
       if (
-        transition.elementId === element.id &&
-        transition.event === TransitionEvent.Add
+        (element.clickEventName || element.rightClickEventName) &&
+        eventHandler
       ) {
-        const transitionClass = getTransitionByType(transition.type);
-        if (!transitionClass) {
-          throw new Error(
-            `Transition class not found for type ${transition.type}`,
+        graphics.eventMode = "static";
+        graphics.on("pointerup", (e) => {
+          if (e.button === 0) {
+            eventHandler(element.clickEventName);
+          } else if (e.button === 2) {
+            eventHandler(element.rightClickEventName);
+          }
+        });
+      }
+
+      if (element.wheelEventName && eventHandler) {
+        graphics.eventMode = "static";
+        graphics.on("wheel", (e) => {
+          eventHandler(element.wheelEventName, {
+            deltaY: e.deltaY,
+          });
+        });
+      }
+
+      const transitionObservables = [];
+
+      for (const transition of transitions) {
+        if (
+          transition.elementId === element.id &&
+          transition.event === TransitionEvent.Add
+        ) {
+          const transitionClass = getTransitionByType(transition.type);
+          if (!transitionClass) {
+            throw new Error(
+              `Transition class not found for type ${transition.type}`,
+            );
+          }
+          transitionObservables.push(
+            transitionClass.add(app, graphics, transition),
           );
         }
-        transitionPromises.push(transitionClass.add(app, graphics, transition));
       }
-    }
 
-    parent.addChild(graphics);
-    await Promise.all(transitionPromises);
+      parent.addChild(graphics);
+
+      const subscription = from(transitionObservables)
+        .pipe(
+          mergeMap((task$) => task$),
+        )
+        .subscribe({
+          error: (err) => {
+            console.error("Error:", err);
+          },
+          complete: () => observer.complete(),
+        });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    });
   };
 
   /**
@@ -128,15 +147,62 @@ export class RectRendererPlugin {
    * @param {RectContainerElement} options.element
    * @param {BaseTransition[]} [options.transitions=[]]
    * @param {Function} options.getTransitionByType
-   * @returns {Promise<undefined>}
+   * @returns {Observable<any>}
    */
-  remove = async (app, options) => {
-    const { parent, element } = options;
-    const sprite = parent.getChildByName(element.id);
-    if (!sprite) {
-      throw new Error(`Sprite with id ${element.id} not found`);
-    }
-    sprite.destroy();
+  remove = (app, options) => {
+    return new Observable((observer) => {
+      const {
+        parent,
+        element,
+        transitions = [],
+        getTransitionByType,
+      } = options;
+      const graphics = parent.getChildByName(element.id);
+      if (!graphics) {
+        throw new Error(`Rect with id ${element.id} not found`);
+      }
+      
+      let transitionObservables = [];
+      for (const transition of transitions) {
+        if (
+          transition.elementId === element.id &&
+          transition.event === TransitionEvent.Remove
+        ) {
+          const transitionClass = getTransitionByType(transition.type);
+          if (!transitionClass) {
+            throw new Error(
+              `Transition class not found for type ${transition.type}`,
+            );
+          }
+          transitionObservables.push(
+            transitionClass.remove(app, graphics, transition),
+          );
+        }
+      }
+
+      const subscription = from(transitionObservables)
+        .pipe(
+          mergeMap((task$) => task$),
+        )
+        .subscribe({
+          error: (err) => {
+            console.error("Error:", err);
+          },
+          complete: () => {
+            observer.complete();
+            if (graphics) {
+              graphics.destroy();
+            }
+          },
+        });
+
+      return () => {
+        subscription.unsubscribe();
+        if (graphics) {
+          graphics.destroy();
+        }
+      };
+    });
   };
 
   /**
@@ -147,39 +213,112 @@ export class RectRendererPlugin {
    * @param {RectContainerElement} options.nextElement
    * @param {BaseTransition[]} [options.transitions=[]]
    * @param {Function} options.getTransitionByType
-   * @returns {Promise<undefined>}
+   * @returns {Observable<undefined>}
    */
-  update = async (app, options) => {
-    const {
+  update = (
+    app,
+    {
       parent,
       prevElement,
       nextElement,
       transitions = [],
       getTransitionByType,
       eventHandler,
-    } = options;
-    const graphics = parent.getChildByName(prevElement.id);
-    if (!graphics) {
-      throw new Error(`Container with id ${prevElement.id} not found`);
-    }
+    },
+  ) => {
+    return new Observable((observer) => {
+      const graphics = parent.getChildByName(prevElement.id);
+      if (!graphics) {
+        throw new Error(`Rect with id ${prevElement.id} not found`);
+      }
 
-    if (JSON.stringify(prevElement) !== JSON.stringify(nextElement)) {
-      await Promise.all([
-        this.remove(app, {
-          parent,
-          element: prevElement,
-          transitions,
-          getTransitionByType,
-          eventHandler,
-        }),
-        this.add(app, {
-          parent,
-          element: nextElement,
-          transitions,
-          getTransitionByType,
-          eventHandler,
-        }),
-      ]);
-    }
+      // Check for update transitions
+      let transitionObservables = [];
+      for (const transition of transitions) {
+        if (
+          transition.elementId === prevElement.id &&
+          transition.event === TransitionEvent.Update
+        ) {
+          const transitionClass = getTransitionByType(transition.type);
+          if (!transitionClass) {
+            throw new Error(
+              `Transition class not found for type ${transition.type}`,
+            );
+          }
+          // For update transitions, we pass the current graphics object
+          transitionObservables.push(
+            transitionClass.add(app, graphics, transition),
+          );
+        }
+      }
+
+      // If there are transitions, apply them
+      if (transitionObservables.length > 0) {
+        const subscription = from(transitionObservables)
+          .pipe(
+            mergeMap((task$) => task$),
+          )
+          .subscribe({
+            error: (err) => {
+              console.error("Error:", err);
+            },
+            complete: () => {
+              // After transitions complete, update any properties not covered by transitions
+              if (prevElement.width !== nextElement.width || prevElement.height !== nextElement.height || prevElement.fill !== nextElement.fill) {
+                graphics.clear();
+                graphics.rect(0, 0, nextElement.width, nextElement.height);
+                graphics.fill(nextElement.fill);
+              }
+              if (prevElement.anchorX !== undefined && nextElement.anchorX !== prevElement.anchorX) {
+                graphics.pivot.x = nextElement.width * nextElement.anchorX;
+              }
+              if (prevElement.anchorY !== undefined && nextElement.anchorY !== prevElement.anchorY) {
+                graphics.pivot.y = nextElement.height * nextElement.anchorY;
+              }
+              observer.complete();
+            },
+          });
+
+        return () => {
+          subscription.unsubscribe();
+        };
+      } else {
+        // No transitions, update properties directly
+        if (prevElement.x !== undefined && nextElement.x !== prevElement.x) {
+          graphics.x = nextElement.x;
+        }
+        if (prevElement.y !== undefined && nextElement.y !== prevElement.y) {
+          graphics.y = nextElement.y;
+        }
+        if (prevElement.alpha !== undefined && nextElement.alpha !== prevElement.alpha) {
+          graphics.alpha = nextElement.alpha;
+        }
+        if (prevElement.scaleX !== undefined && nextElement.scaleX !== prevElement.scaleX) {
+          graphics.scale.x = nextElement.scaleX;
+        }
+        if (prevElement.scaleY !== undefined && nextElement.scaleY !== prevElement.scaleY) {
+          graphics.scale.y = nextElement.scaleY;
+        }
+        if (prevElement.rotation !== undefined && nextElement.rotation !== prevElement.rotation) {
+          graphics.rotation = (nextElement.rotation * Math.PI) / 180;
+        }
+
+        if (prevElement.width !== nextElement.width || prevElement.height !== nextElement.height || prevElement.fill !== nextElement.fill) {
+          graphics.clear();
+          graphics.rect(0, 0, nextElement.width, nextElement.height);
+          graphics.fill(nextElement.fill);
+        }
+
+        if (prevElement.anchorX !== undefined && nextElement.anchorX !== prevElement.anchorX) {
+          graphics.pivot.x = nextElement.width * nextElement.anchorX;
+        }
+        if (prevElement.anchorY !== undefined && nextElement.anchorY !== prevElement.anchorY) {
+          graphics.pivot.y = nextElement.height * nextElement.anchorY;
+        }
+
+        observer.complete();
+        return () => {};
+      }
+    });
   };
 }
